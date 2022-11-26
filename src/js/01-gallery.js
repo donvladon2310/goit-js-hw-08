798191e8c83841b0d1e633c365bdda9fbdbd8197
donvladon2310 <vibliy.vlad@gmail.com>
2022-11-26 23:21:57 +0200

    hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from 'simplelightbox';
+// @ts-ignore
+import SimpleLightbox from 'simplelightbox'; // import library
+// // @ts-ignore
+// import * as Simplelightbox from "simplelightbox";
+
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const documentGallery = document.querySelector('.gallery');
+const imageCard = onCreateGalleryItem(galleryItems);
+
+documentGallery.insertAdjacentHTML('beforeend', imageCard);
+
+function onCreateGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image lazyload"
+        data-src="${preview}"
+        alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  disableRightClick: true,
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
